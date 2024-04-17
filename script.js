15e020034f34bc3a3b626247c17ff5042505f8de
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-17 15:22:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Variabili
+let tariffaKM = prompt ("Inserisci il tuo nome: ");
+let sconti = prompt ("Inserisci il tuo colore preferito: ");
+
+//Variabile password
+let  = "La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24";
+
+document.getElementById("titolo").innerHTML = ("La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24"); //primo caso che ho fatto
+
+alert(password24); //nel caso dell'alert (l'ho fatto al contrario rispetto all'ordine che ha dato Luca)
+
+
+
+let distanzaKM = prompt ("Inserisci la distanza (in KM): ");
+let eta = prompt ("Inserisci la tua età: ");
