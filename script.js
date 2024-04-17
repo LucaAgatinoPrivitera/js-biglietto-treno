7e64bdb8bf0009b9bff34af3a45bf6b96538489c
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-17 15:37:23 +0200

    Sconti per minorenni fatti

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Variabili
+/*
 let tariffaKM = prompt ("Inserisci il tuo nome: ");
 let sconti = prompt ("Inserisci il tuo colore preferito: ");
 
@@ -8,8 +9,17 @@ let  = "La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "
 document.getElementById("titolo").innerHTML = ("La tua password è: " + NomeUtente + CognomeUtente + ColorePreferito + "#24"); //primo caso che ho fatto
 
 alert(password24); //nel caso dell'alert (l'ho fatto al contrario rispetto all'ordine che ha dato Luca)
+*/
 
 
+const distanzaKM = prompt ("Inserisci la distanza (in KM): ");
+const eta = prompt ("Inserisci la tua età: ");
 
-let distanzaKM = prompt ("Inserisci la distanza (in KM): ");
-let eta = prompt ("Inserisci la tua età: ");
+const costoKM = 100
+let prezzo = costoKM * distanzaKM
+
+if (eta<18){
+    prezzo = (80 / 100) * prezzo
+}
+
+console.log(prezzo);
