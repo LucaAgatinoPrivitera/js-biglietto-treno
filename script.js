cf3ea228011e303c54d279e6eea26b92e7295947
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-17 15:46:07 +0200

    Esercitazione finita

--- script.js
+++ script.js
@@ -11,15 +11,25 @@ document.getElementById("titolo").innerHTML = ("La tua password è: " + NomeUten
 alert(password24); //nel caso dell'alert (l'ho fatto al contrario rispetto all'ordine che ha dato Luca)
 */
 
-
+// Variabili
 const distanzaKM = prompt ("Inserisci la distanza (in KM): ");
 const eta = prompt ("Inserisci la tua età: ");
-
 const costoKM = 100
+
 let prezzo = costoKM * distanzaKM
 
+// Sconti minorenni
 if (eta<18){
     prezzo = (80 / 100) * prezzo
 }
+// Sconti senior
+if (eta>65){
+    prezzo = (60 / 100) * prezzo
+}
 
-console.log(prezzo);
+//Prezzo finale, con massimo due numeri dopo la virgola
+prezzofin = prezzo.toFixed(2);
+
+//Stampa
+console.log(`La quota da pagare, per poter viaggire è di: ${prezzofin} Euro.`);
+document.getElementById("titolo").innerHTML = (`Il prezzo da pagare è: ${prezzofin} Euro.`);
